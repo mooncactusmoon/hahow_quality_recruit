c147c5b96b5b590db9d767e56b35da2cf8d8c59e
moon <mooncactusmoon@yahoo.com>
2024-03-16 21:10:54 +0800

    結構化API與UI測試的答案

--- Api_test/api_test.py
+++ Api_test/api_test.py
@@ -1,48 +1,77 @@
 import requests
 
-swapi = requests.get("https://swapi.dev/api/")
-swapi_data = swapi.json()
 
-all_films = requests.get(swapi_data["films"]).json()
-films_results = all_films["results"]
+def get_species_count_in_episode(episode_number):
+    """
+    取得指定集數電影中出現的不同種族數量
+    
+    Args:
+        episode_number: 集數
 
-## 有多少不同種族的人出現在第六部？
-for i in range(len(films_results)):
-    if films_results[i]['episode_id'] == 6:
-        species_list = films_results[i]['species']
-        #若 species Api 沒有重複的種族名稱，則可直接寫 "len(films_results[i]['species'])" 來取得總數
+    Returns:
+        不同種族數量
+    """
 
-# 不確定species Api 是否有重複的種族名稱，因此以下多做此判斷處理 (利用元組可以直接濾掉重複的值之特性)
-species_name_list = []
+    episode_data = next(
+        (film for film in films_results if film["episode_id"] == episode_number), None
+    )
+    if episode_data:
+        species_urls = episode_data["species"]
+        species_names = set(requests.get(url).json()["name"] for url in species_urls)
+        return len(species_names)
+    return 0
 
-for i in range(len(species_list)):
-    species_name = requests.get(species_list[i]).json()['name']
-    species_name_list.append(species_name)
 
-species_name_list_total = len(list(set(species_name_list)))
+def print_films_in_order():
+    """
+    依據電影集數去排序電影名字
+    """
 
-print(f'總共有 "{species_name_list_total}" 種不同種族的人出現在第六部')
+    films_list = {film["episode_id"]: film["title"] for film in films_results}
+    for episode_number, title in sorted(films_list.items()):
+        print(f"第 {episode_number} 集電影名稱為 : {title}")
 
-## 請依據電影集數去排序電影名字？
-films_list = {}
 
-for i in range(len(films_results)):
-    films_list[films_results[i]['episode_id']] = films_results[i]['title']
+def find_powerful_vehicles(films_results):
+    """
+    找出電影裡所有的車輛，馬力超過1000的
 
-for i in range(len(films_results)):
-    print(f'第 {i+1} 集電影名稱為 : {films_list[i+1]}')
+    Args:
+        films_results: 電影資料
 
-## 請幫我挑出電影裡所有的車輛，馬力超過１０００的。
-films_vehicles = []
+    Returns:
+        馬力超過1000的車輛
+    """
 
-for i in range(len(films_results)):
-    for j in range(len(films_results[i]['vehicles'])):
-        films_vehicles.append(films_results[i]['vehicles'][j])
+    powerful_vehicles = set()
+    for film in films_results:
+        for vehicle_url in film["vehicles"]:
+            vehicle_data = requests.get(vehicle_url).json()
+            max_speed = vehicle_data.get("max_atmosphering_speed", "")
+            
+            if max_speed.isdigit() and int(max_speed) > 1000:
+                powerful_vehicles.add(vehicle_data["name"])
+    return powerful_vehicles
 
-films_vehicles_list = list(set(films_vehicles))
 
-for i in range(len(films_vehicles_list)):
-    vehicles = requests.get(films_vehicles_list[i]).json()
-    if str.isdigit(vehicles['max_atmosphering_speed']):
-        if int(vehicles['max_atmosphering_speed']) > 1000:
-            print(f'{vehicles["name"]} (馬力為 {vehicles["max_atmosphering_speed"]})', end = '、')
+if __name__ == "__main__":
+    """
+    主程式
+    """
+
+    # 取得 Star Wars API 資料
+    swapi = requests.get("https://swapi.dev/api/")
+    swapi_data = swapi.json()
+    all_films = requests.get(swapi_data["films"]).json()
+    films_results = all_films["results"]
+
+    # 有多少不同種族的人出現在第六部
+    species_count = get_species_count_in_episode(6)
+    print(f'總共有 "{species_count}" 種不同種族的人出現在第六部')
+
+    # 依據電影集數去排序電影名字
+    print_films_in_order()
+
+    # 找出電影裡所有的車輛，馬力超過1000的
+    powerful_vehicles = find_powerful_vehicles(films_results)
+    print("馬力超過1000的車輛：", "、".join(powerful_vehicles))
--- Ui_test/ui_test.py
+++ Ui_test/ui_test.py
@@ -1,63 +1,57 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import numpy as np
-import PIL.Image
-import PIL.ImageEnhance
 import cv2
 import time
 
+
+def list_contributors(driver):
+    """此專案裡有幾個合作者，並且分別列出他們的名字"""
+    driver.get('https://github.com/hahow/hahow-recruit')
+    contributors_title_link = driver.find_element(By.XPATH,
+                                                  '//*[@id="repo-content-pjax-container"]/div/div/div[2]/div[2]/div/div[5]/div/h2/a')
+    contributors_total = int(contributors_title_link.find_element(By.XPATH, './span').text)
+    print(f'此專案總共有 {contributors_total} 位合作者，名單如下：')
+    contributors_title_link.click()
+    contributors_list = driver.find_elements(By.XPATH, '//a[@data-hovercard-type="user"][@class="text-normal"][text()]')
+
+    for person in contributors_list:
+        print(person.text)
+
+
+def check_wireframe_images(driver):
+    """檢查Wireframe的圖片是否存在。"""
+    driver.get('https://github.com/hahow/hahow-recruit/blob/master/frontend.md')
+    time.sleep(2)
+    wireframe_image1 = driver.find_element(By.XPATH,
+                                           '//img[@src="/hahow/hahow-recruit/raw/master/assets/hero-list-page.png"]')
+    wireframe_image2 = driver.find_element(By.XPATH,
+                                           '//img[@src="/hahow/hahow-recruit/raw/master/assets/hero-profile-page.png"]')
+
+    wireframe_image1.screenshot('Ui_test/compare/hero-list-page.png')
+    wireframe_image2.screenshot('Ui_test/compare/hero-profile-page.png')
+
+    compare_images_pixel('Ui_test/assets/hero-list-page.png', 'Ui_test/compare/hero-list-page.png')
+    compare_images_pixel('Ui_test/assets/hero-profile-page.png', 'Ui_test/compare/hero-profile-page.png')
+
+
 def compare_images_pixel(compare1, compare2):
-    """比較兩張圖的像素，差異像素設定為不超過100"""
-    file_name = compare1.split('//')[-1]
-    # 讀取兩張圖片
+    """比較兩張圖片的像素差異，如果差異像素少於100則認為圖片存在。"""
+    file_name = compare1.split('/')[-1]
     img1 = cv2.imread(compare1)
     img2 = cv2.imread(compare2)
-    
-    # 比較所有像素的不同
     diff_image = cv2.absdiff(img1, img2)
     diff_pixels = np.sum(diff_image)
     different_pixel_count = np.count_nonzero(diff_pixels)
+
     if different_pixel_count < 100:
         print(f'{file_name} 圖片存在')
     else:
         print(f'{file_name} 圖片不存在')
 
 
-if __name__ == '__main__':
-    
-    driver = webdriver.Chrome()
-    driver.get('https://github.com/hahow/hahow-recruit')
-    driver.implicitly_wait(5) #先用隱性等待、之後再看是否要改成顯性
-
-    ## 此專案裡有幾個合作者，並且分別列出他們的名字
-    contributors_title_link = driver.find_element(By.XPATH, '//*[@id="repo-content-pjax-container"]/div/div/div[2]/div[2]/div/div[5]/div/h2/a')
-    contributors_total = int(driver.find_element(By.XPATH, '//*[@id="repo-content-pjax-container"]/div/div/div[2]/div[2]/div/div[5]/div/h2/a/span').text)
-    print(f'此專案總共有 {contributors_total} 位合作者，名單如下：')
-    contributors_title_link.click()
-
-    contributors_list = driver.find_elements(By.XPATH, '//a[@data-hovercard-type="user"][@class="text-normal"][text()]')
-    for person in contributors_list:
-        print(person.text)
-
-    ## 請進入到 frontend.md 並且查看 "Wireframe" 的圖片是否存在
-    driver.get('https://github.com/hahow/hahow-recruit/blob/master/frontend.md')
-    wireframe_image1 = driver.find_element(By.XPATH, '//img[@src="/hahow/hahow-recruit/raw/master/assets/hero-list-page.png"]')
-    wireframe_image2 = driver.find_element(By.XPATH, '//img[@src="/hahow/hahow-recruit/raw/master/assets/hero-profile-page.png"]')
-    time.sleep(2)
-    wireframe_image1.screenshot('Ui_test//compare//hero-list-page.png')
-    time.sleep(2)
-    wireframe_image2.screenshot('Ui_test//compare//hero-profile-page.png')
-    wireframe_image1_path = 'Ui_test//compare//hero-list-page.png'
-    wireframe_image2_path = 'Ui_test//compare//hero-profile-page.png'
-    
-    expect_image1 = 'Ui_test//assets//hero-list-page.png'
-    expect_image2 = 'Ui_test//assets//hero-profile-page.png'
-    
-    compare_images_pixel(expect_image1, wireframe_image1_path)
-    compare_images_pixel(expect_image2, wireframe_image2_path)
-
-
-    ## 最後一個 commit 的人是誰
+def find_last_non_merge_commit(driver):
+    """找出最後一個 commit 的人是誰"""
     driver.get('https://github.com/hahow/hahow-recruit/commits/master/')
     all_commits = driver.find_elements(By.XPATH, '//div[@data-testid="listview-item-title-container"]')
     merge_commit = driver.find_element(By.XPATH, '//*[text()="Merge pull request"]')
@@ -68,3 +62,18 @@ if __name__ == '__main__':
         if merge_commit.text not in commit.text:
             print(f'最後一個 commit 的人是 {commits_person[i].text}， commit 標題是: {commit.text}')
             break
+
+
+if __name__ == '__main__':
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(5)
+
+    # 列出所有合作者及數量
+    list_contributors(driver)
+
+    # 確認 Wireframe 圖是否存在
+    check_wireframe_images(driver)
+
+    # 找到最後一個 commit 的人
+    find_last_non_merge_commit(driver)
+    driver.quit()
